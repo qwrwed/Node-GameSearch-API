55f6e4aff3a32cef9dee4e56c5528038d83822a3
qwrwed <qwrwed@users.noreply.github.com>
2019-04-13 20:32:35 +0100

    changed information source to igdb

--- app.js
+++ app.js
@@ -6,64 +6,145 @@ app.use(bodyParser.json());
 app.use(express.static('client'));
 
 const fetch = require("node-fetch");
-
-const sample = require('./sample_data.json');
+//const primaryDataLocation = 'http://www.recipepuppy.com/api/?i=potato'
+//const primaryDataLocation = 'http://www.recipepuppy.com/api/?i='
+const primaryDataLocation = "https://api-v3.igdb.com/games";
+const api_key = "***REMOVED***";
+const secondaryDataLocation = "./sample_data.json";
 
 let data_list;
 
 async function initData(){
-    let data_raw;
 
-    let response = await fetch('http://www.recipepuppy.com/api/?i=potato');
+    try {
 
-    if (response.ok) {
-        data_raw = await response.text();
-    } else {
-        throw new Error('404 Remote Page Not Found')
+        let response = await fetch(primaryDataLocation, {
+            headers: {
+                "user-key": api_key,
+                Accept: "application/json"
+            },
+            method:"POST",
+            //body:`fields name; search "halo";`
+            //body:`fields name; search "*";`
+            //body: `fields *; where platforms = 48; sort popularity desc; limit 50;`
+            body: `fields id, name, summary, storyline, platforms.name, genres.name, cover.image_id; where platforms = (48, 49); sort popularity desc; limit 10;`
+            //body: `fields name; where platforms = (48, 49); sort popularity desc; limit 50;`
+
+        });
+
+        let data_json;
+        if (response.ok) {
+            data_json = await response.json();
+        } else {
+            throw new Error('404 Remote Page Not Found')
+        }
+
+        return (data_json)
+    } catch(e) {
+        console.log("Error: " + e.message);
+        console.log("Using backup dataset instead.");
+        return(require(secondaryDataLocation));
     }
-
-    const data_parsed = JSON.parse(data_raw);
-    const data = data_parsed.results;
-
-    return(data)
 }
 
-function stringifyEntry(entry){
-    s = "";
-    s += `Title: ${entry.title} <br>`;
-    s += `Ingredients: ${entry.ingredients} <br>`;
-    s += `Link: <a href ='${entry.href}'>${entry.href}</a><br>`;
-    s += `<img src='${entry.thumbnail}'><br>`;
-    return(s)
 
-}
-
-//data_list = sample;
+//initialise the data
 initData().then(data => data_list = data);
 
+
+//GET method to list/search
 app.get('/search', async function (req, resp){
 
     const key = req.query.key;
-
     let entry;
     let resp_list = [];
+
     for (i = 0; i < data_list.length; i++){
         entry = data_list[i];
-        if (entry.title.toLowerCase().includes(key)) {
-            resp_list.push(entry)
+        //console.log(entry)
+        if (entry.name.toLowerCase().includes(key)) {
+            resp_list.push({name : entry.name})
         }
     }
+
+    //Response provided as JSON
     resp.send(resp_list);
 
 });
 
+//GET method for individual details
+app.get('/entry', async function (req, resp){
+    //Response provided as JSON
+    resp.send(data_list[req.query.id]);
+});
+
+function validateField(key, value) {
+    //key passed in to allow specific checks for specific fields
+    let valid = true;
+    if (value === "" || typeof(value) === "undefined") {
+        valid = false;
+    }
+    return(valid)
+}
+
+app.get('/test', (req, res) => {
+    //console.log(res)
+    /*
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, Content-Type, Accept"
+    )*/
+    fetch("https://api-v3.igdb.com/games", {
+        headers: {
+            "user-key": api_key,
+            Accept: "application/json"
+        },
+        method:"POST",
+        //body:`fields name; search "halo";`
+        //body:`fields name; search "*";`
+        //body: `fields *; where platforms = 48; sort popularity desc; limit 50;`
+        body: `fields id, name, summary, storyline, platforms.name, genres.name; where platforms = (48, 49); sort popularity desc; limit 50;`
+        //body: `fields name; where platforms = (48, 49); sort popularity desc; limit 50;`
+
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            //console.log(response.length)
+            res.send(response);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+});
+
+//POST method to add new
+//TODO: Add authentication
 app.post('/add', async function(req, resp){
+    const body = req.body;
+    let validRequest = true;
+    let invalidFields = [];
+    let entry = {};
+    for (let field in body) {
+        if (!validateField(field, body[field].value)) {
+            validRequest = false;
+            invalidFields.push(body[field].label);
+        } else {
+            entry[field] = body[field].value;
+        }
+    }
 
-    data_list.push(req.body);
-
-    let s = `Entry received:<br><br>`;
-    s += stringifyEntry(req.body);
-    resp.send(s);
+    if (validRequest) {
+        data_list.push(entry);
+        //Response provided as JSON
+        resp.send(entry);
+    } else {
+        resp.status(400);
+        resp.statusMessage = `Invalid Request: Invalid value(s) for field(s): ${invalidFields}`;
+        resp.send();
+    }
 
 });
 
--- client/index.js
+++ client/index.js
@@ -30,18 +30,99 @@ function openTab(event, tabName) {
     event.currentTarget.className += " active";
 }
 
-
-function stringifyEntry(entry){
-    s = "";
-    s += `Title: ${entry.title} <br>`;
-    s += `Ingredients: ${entry.ingredients} <br>`;
-    s += `Link: <a href ='${entry.href}'>${entry.href}</a><br>`;
-    s += `<img src='${entry.thumbnail}'><br>`;
-    return(s)
-
+function capitalise(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+function getComponent(object, component) {
+    let result = [];
+    for (let i = 0; i < object.length; i++) {
+        result.push(object[i][component])
+    }
+    return(result)
+}
+
+function stringifyEntry(entry) {
+
+    let s = "";
+    //console.log(entry);
+    s += `<h1>${entry.name}</h1>`;
+    s += `Platforms: ${getComponent(entry.platforms, "name").join(", ")}<br>`;
+    s += `Genres: ${getComponent(entry.genres, "name").join(", ")}<br>`;
+    s += `<img src="https://images.igdb.com/igdb/image/upload/t_cover_big/${entry.cover.image_id}.jpg"><br>`;
+    //s += `<img src='${entry.cover.url}'><br>`;
+
+    /*
+    const order = [0, 2, 1];
+    entryKeys = Object.keys(entry);
+    console.log(entryKeys.length)
+    let foo = [ ...Array(entryKeys.length).keys() ].slice(order.length);
+    order.push(...foo)
+    console.log(order)
+    //console.log(entry);
+    //console.log(entryKeys)
+
+
+    let s = "";
+    for (let i = 0; i < order.length; i++) {
+        key = entryKeys[order[i]];
+        console.log(key)
+        s += `${capitalise(key)}: `;
+        if (typeof(entry[key]) === "object") {
+            for (let j = 0; j < entry[key].length; j++) {
+
+                if (j > 0) {
+                    s += ", ";
+                }
+                s += entry[key][j].name;
+            }
+        } else {
+            s += entry[key];
+        }
+        s += `<br>`
+        //s += `Title: ${entry.title} <br>`;
+        //s += `Ingredients: ${entry.ingredients} <br>`;
+        //s += `Link: <a href ='${entry.href}'>${entry.href}</a><br>`;
+        //s += `<img src='${entry.thumbnail}'><br>`;
+    }*/
+    return(s)
+}
+
+
+
+
+function defineLinks(data_list) {
+    for (i=0;i<data_list.length;i++) {
+            document.getElementById(`entry_${i}`).addEventListener("click", async function (event) {
+            event.preventDefault();
+            const id = event.target.id.replace("entry_", "");
+            try {
+
+                const response = await fetch(`http://127.0.0.1:8090/entry?id=${id}`);
+
+                if (response.ok) {
+
+                    let s;
+
+                    const entry = await response.json();
+                    //const entry = JSON.parse(body);
+                    //console.log(entry)
+                    s = stringifyEntry(entry)
+                    document.getElementById('content').innerHTML = s;
+
+                } else {
+                    throw new Error('404 Not Found')
+                }
+            } catch (e) {
+                console.log(e);
+                alert(e);
+            }
+
+        })
+    }
+}
+
+document.addEventListener('DOMContentLoaded', async function() {
 
     const searchForm = document.getElementById("searchForm");
     searchForm.addEventListener('submit', async function (event) {
@@ -51,7 +132,6 @@ document.addEventListener('DOMContentLoaded', function() {
         try {
 
             const response = await fetch(`http://127.0.0.1:8090/search?key=${key}`);
-
             if (response.ok) {
 
                 let s;
@@ -65,10 +145,12 @@ document.addEventListener('DOMContentLoaded', function() {
                 const data_list = JSON.parse(body);
 
                 for (i = 0; i < data_list.length; i++){
-                    s += stringifyEntry(data_list[i]);
-                    s += `<br>`;
+                    s += `<a href ="http://127.0.0.1:8090/entry?id=${i}" id="entry_${i}" >${data_list[i].name}</a><br>`;
                 }
                 document.getElementById('content').innerHTML = s;
+
+                defineLinks(data_list);
+
             } else {
                 throw new Error('404 Not Found')
             }
@@ -78,38 +160,49 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    const addForm = document.getElementById("addForm");
 
+
+
+    const addForm = document.getElementById("addForm");
+    //console.log(addForm.hasOwnProperty("tatle"))
     addForm.addEventListener("submit", async function (event) {
         event.preventDefault();
-        let validEntry = true;
+
         let formData = {};
-        for (let i = 0; i < addForm.length-1; i++) {
-            if (addForm[i].value === "") {
-                validEntry = false
-            } else {
-                formData[addForm[i].id] = addForm[i].value;
+        let labels = document.getElementsByTagName('label');
+        let id;
+
+        for (let i = 0; i < labels.length; i++) {
+            id = labels[i].htmlFor;
+            if (addForm.hasOwnProperty(id)) {
+                formData[id] = {
+                    value: addForm[i].value,
+                    label: labels[i].innerHTML
+                }
             }
         }
-        if (validEntry === true) {
-            try {
-                let response = await fetch("http://127.0.0.1:8090/add",{
-                    method: "POST",
-                    headers:{'Content-Type': 'application/json'},
-                    body: JSON.stringify(formData)
-                });
-                if (response.ok) {
-                    let resp_text = await response.text();
-                    document.getElementById('content').innerHTML = resp_text;
-                } else {
-                    throw new Error('404 Not Found')
-                }
-            } catch (e) {
-                console.log(e);
-                alert(e);
+
+        try {
+            let response = await fetch("http://127.0.0.1:8090/add",{
+                method: "POST",
+                //Content-type needs to be correct:
+                headers:{'Content-Type': 'application/json'},
+                body: JSON.stringify(formData)
+            });
+            if (response.ok) {
+                const body = await response.text();
+                const resp = JSON.parse(body);
+
+                let s = `Entry received:<br>`;
+                s += stringifyEntry(resp);
+
+                document.getElementById('content').innerHTML = s;
+            } else {
+                throw new Error(response.status + " " + response.statusText)
             }
-        } else {
-            document.getElementById('content').innerHTML = "Entry not received: You must fill all fields before submitting.";
+        } catch (e) {
+            console.log(e);
+            alert(e);
         }
 
     });
